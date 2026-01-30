2eb56953dcbfaf687e6e3dc3f0aac2a7e3647f70
danixaulin-droid <danixaulin@gmail.com>
2026-01-30 05:21:43 -0300

    Update HideOnRoutes.jsx

--- components/HideOnRoutes.jsx
+++ components/HideOnRoutes.jsx
@@ -2,13 +2,8 @@
 
 import { usePathname } from "next/navigation";
 
-export default function HideOnRoutes({
-  hidePrefixes = [],
-  children,
-  render,
-}) {
+export default function HideOnRoutes({ hidePrefixes = [], children, render }) {
   const pathname = usePathname() || "/";
-
   const isHidden = hidePrefixes.some((p) => pathname.startsWith(p));
 
   if (typeof render === "function") {
